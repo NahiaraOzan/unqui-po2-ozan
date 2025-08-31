2f8cea2dd4cb19389222e9bec74fc99cee872a8c
NahiaraOzan <nahiara_ozan@hotmail.com>
2025-08-31 14:42:48 -0300

    ultm version

--- src/ar/edu/unq/po2/tp3/Punto.java
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -12,14 +12,6 @@ public class Punto {
 		this.setX(x);
 		this.setY(y);
 	}
-	//Nahiara tiene hambre
-	//fdfdfasfsdf
-	//fdfdfasfsdf
-	//fdfdfasfsdf
-	//fdfdfasfsdf
-	//fdfdfasfsdf
-	//fdfdfasfsdf
-	
 	
 	public float getX() {
 		return x;
