8578dd55d0d9a3d66217751ceddf7f2bd6a082bf
NahiaraOzan <nahiara_ozan@hotmail.com>
2025-09-27 23:06:26 -0300

    tp6

--- src/ar/edu/unq/po2/tp3/Punto.java
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -3,6 +3,7 @@ package ar.edu.unq.po2.tp3;
 public class Punto {
 	private float x;
 	private float y;
+	
 	public Punto(float x, float y) {
 		super();
 		this.setXY(x,y);
@@ -26,4 +27,17 @@ public class Punto {
 		this.y = y;
 	}
 	
+	public Punto() {
+		this.setXY(0,0);
+	}
+	
+	public void sumarPuntos(Punto punto) {
+		float nuevoX = punto.getX() + this.x ;
+		float nuevoY = punto.getY() + this.y ;
+		this.setXY(nuevoX, nuevoY);
+	}
+	
+	public void moverHacia(float x, float y) {
+		this.setXY(x, y);
+	}
 }
